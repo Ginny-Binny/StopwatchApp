7a9f3c4ceb6ec112ded8bca899ae409c716a3dbb
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-02-13 10:14:11 +0530

    multiplevalues

---
+++ test codes/r/multiplevalues.r
@@ -0,0 +1,8 @@
+b=c(2,3,4,5,1)
+cat(b)
+
+new_b <- c(1,2,"HJ", 5,6) #thiswill make the whole b to string
+print(new_b)
+print(class(new_b))  
+
+#if we want to overcome this issue than we can use only homogenous num types.
