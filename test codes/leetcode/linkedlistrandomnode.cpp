ea080796e85512d5f18759b1db9e20bda7faef85
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-03-10 19:55:51 +0530

    d20

---
+++ test codes/leetcode/linkedlistrandomnode.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    ListNode* head;
+public:
+    Solution(ListNode* head) {
+        this->head = head;
+        srand(time(NULL));
+    }
+    
+    int getRandom() {
+        ListNode* node = head;
+        int count = 1;
+        int result = 0;
+        while (node) {
+            if (rand() % count == 0) {
+                result = node->val;
+            }
+            count++;
+            node = node->next;
+        }
+        return result;
+    }
+};
