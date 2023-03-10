2212f455146341404ff38451578859e46ef8fa03
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-03-10 20:01:16 +0530

    error#

--- test codes/leetcode/linkedlistrandomnode.cpp
+++ test codes/leetcode/linkedlistrandomnode.cpp
@@ -1,14 +1,13 @@
 #include <cstdlib>
 #include <ctime>
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+    };
+
 
 class Solution {
 private:
