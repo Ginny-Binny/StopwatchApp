d407b04ea6d8c550d04fbe95da0dfa7ceabc2ced
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-03-11 21:03:39 +0530

    d21

--- test codes/leetcode/sortedlisttobinarysearchtree.cpp
+++ test codes/leetcode/sortedlisttobinarysearchtree.cpp
@@ -1,3 +1,21 @@
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 public:
     TreeNode* sortedListToBST(ListNode* head) {
@@ -29,3 +47,28 @@ public:
     }
 };
 
+// Test the solution
+int main() {
+    ListNode* head = new ListNode(-10);
+    head->next = new ListNode(-3);
+    head->next->next = new ListNode(0);
+    head->next->next->next = new ListNode(5);
+    head->next->next->next->next = new ListNode(9);
+    
+    Solution sol;
+    TreeNode* root = sol.sortedListToBST(head);
+    
+    // Test if the constructed tree is a binary search tree
+    // Inorder traversal should return sorted values
+    function<void(TreeNode*)> inorder = [&](TreeNode* node) {
+        if (!node) {
+            return;
+        }
+        inorder(node->left);
+        cout << node->val << " ";
+        inorder(node->right);
+    };
+    inorder(root); // expected output: -10 -3 0 5 9 
+    
+    return 0;
+}
