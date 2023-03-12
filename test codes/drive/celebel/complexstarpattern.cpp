53b09e8396f8f1729082f8353a5e03a9ce57220e
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-03-12 16:29:15 +0530

    q2

--- test codes/drive/celebel/complexstarpattern.cpp
+++ test codes/drive/celebel/complexstarpattern.cpp
@@ -1,40 +1,26 @@
-#include <iostream>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-void printComplexStarPattern(int n) {
-    // printing upper triangle
-    for (int i = 1; i <= n; i++) {
-        // printing spaces
-        for (int j = 1; j <= n-i; j++) {
-            cout << " ";
-        }
-        // printing stars
-        for (int j = 1; j <= i; j++) {
-            cout << "* ";
-        }
-        cout << endl;
-    }
-
-    // printing lower triangle
-    for (int i = n-1; i >= 1; i--) {
-        // printing spaces
-        for (int j = 1; j <= n-i; j++) {
-            cout << " ";
-        }
-        // printing stars
-        for (int j = 1; j <= i; j++) {
-            cout << "* ";
-        }
-        cout << endl;
-    }
-}
-
-
 int main() {
-    int n;
-    cin >> n;
+    int n; cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = n; j > i; j--) {
+            cout<<" ";
+        }
+        for (int k = 1; k <= i; k++) {
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
+    for (int i = 1; i < n; i++) {
+        for (int k = 1; k <= i; k++) {
+            cout<<" ";
+        }
+        for (int j = n; j > i; j--) {
+            cout<<"* ";
+        }
+        cout<<"\n";
+    }
 
-    printComplexStarPattern(n);
-
-    return 0;
 }
