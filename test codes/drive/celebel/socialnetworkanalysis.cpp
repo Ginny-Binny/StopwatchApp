e1d0dc0a2af8e75672c3b7d71e968a6e0878f9ea
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-03-12 16:23:55 +0530

    method 1

---
+++ test codes/drive/celebel/socialnetworkanalysis.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, e; cin >> n >> e;
+    unordered_map<int, vector<int>> mp;
+    size_t maxSize = 0;
+    for (int i = 0; i < e; i++) {
+        int n1, n2; cin >> n1 >> n2;
+        mp[n1].push_back(n2);
+        mp[n2].push_back(n1);
+        maxSize = max(maxSize, max(mp[n1].size(), mp[n2].size()));
+    }
+    vector<int> results;
+    for (auto p : mp) {
+        int k = p.first;
+        if (p.second.size() == maxSize) {
+            results.push_back(k);
+        }
+    }
+    sort(begin(results), end(results));
+    for (int val : results) {
+        cout << val << " ";
+    }
+    cout<<"\n";
+}
