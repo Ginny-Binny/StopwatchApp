b78e95baf347e3ee0f2691c4d58461904551c620
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-03-12 16:20:37 +0530

    contest

--- test codes/leetcode/sortedlisttobinarysearchtree.cpp
+++ test codes/leetcode/sortedlisttobinarysearchtree.cpp
@@ -1,68 +1,27 @@
-#include <iostream>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
-class Solution {
-public:
-    TreeNode* sortedListToBST(ListNode* head) {
-        if (!head) {
-            return NULL;
-        }
-        return sortedListToBST(head, NULL);
+int main()
+{
+    int n, e; cin >> n >> e;
+    unordered_map<int, vector<int>> mp;
+    size_t maxSize = 0;
+    for (int i = 0; i < e; i++) {
+        int n1, n2; cin >> n1 >> n2;
+        mp[n1].push_back(n2);
+        mp[n2].push_back(n1);
+        maxSize = max(maxSize, max(mp[n1].size(), mp[n2].size()));
     }
-    
-    TreeNode* sortedListToBST(ListNode* head, ListNode* tail) {
-        if (head == tail) {
-            return NULL;
+    vector<int> results;
+    for (auto p : mp) {
+        int k = p.first;
+        if (p.second.size() == maxSize) {
+            results.push_back(k);
         }
-        ListNode* mid = findMid(head, tail);
-        TreeNode* root = new TreeNode(mid->val);
-        root->left = sortedListToBST(head, mid);
-        root->right = sortedListToBST(mid->next, tail);
-        return root;
     }
-    
-    ListNode* findMid(ListNode* head, ListNode* tail) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while (fast != tail && fast->next != tail) {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        return slow;
-    }
-};
-
-int main() {
-    ListNode* head = new ListNode(-10);
-    head->next = new ListNode(-3);
-    head->next->next = new ListNode(0);
-    head->next->next->next = new ListNode(5);
-    head->next->next->next->next = new ListNode(9);
-    
-    Solution sol;
-    TreeNode* root = sol.sortedListToBST(head);
-    function<void(TreeNode*)> inorder = [&](TreeNode* node) {
-        if (!node) {
-            return;
-        }
-        inorder(node->left);
-        cout << node->val << " ";
-        inorder(node->right);
-    };
-    inorder(root);  
-    
-    return 0;
+    sort(begin(results), end(results));
+    for (int val : results) {
+        cout << val << " ";
+    }cout<<"\n";
 }
