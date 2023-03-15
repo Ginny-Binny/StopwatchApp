3b5617702338b7535c834ee15a37ee3e38bc230d
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-03-15 23:57:31 +0530

    25

--- test codes/leetcode/completenessofbinarytree.cpp
+++ test codes/leetcode/completenessofbinarytree.cpp
@@ -1,47 +1,39 @@
-#include <queue>
+class Solution {
+public:
+    bool isCompleteTree(TreeNode* root) {
+        if (!root) {
+            return true;
+        }
 
-using namespace std;
+        queue<TreeNode*> q;
+        q.push(root);
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+        bool reachedEnd = false;
+        while (!q.empty()) {
+            TreeNode* curr = q.front();
+            q.pop();
 
-bool isCompleteTree(TreeNode* root) {
-    if (!root) {
+            if (reachedEnd && (curr->left || curr->right)) {
+                return false;
+            }
+
+            if (!curr->left && curr->right) {
+                return false;
+            }
+
+            if ((curr->left && !curr->right) || (!curr->left && !curr->right)) {
+                reachedEnd = true;
+            }
+
+            if (curr->left) {
+                q.push(curr->left);
+            }
+
+            if (curr->right) {
+                q.push(curr->right);
+            }
+        }
+
         return true;
     }
-    
-    queue<TreeNode*> q;
-    q.push(root);
-    
-    bool reachedEnd = false;
-    while (!q.empty()) {
-        TreeNode* curr = q.front();
-        q.pop();
-        
-        if (reachedEnd && (curr->left || curr->right)) {
-            return false;
-        }
-        
-        if (!curr->left && curr->right) {
-            return false;
-        }
-        
-        if ((curr->left && !curr->right) || (!curr->left && !curr->right)) {
-            reachedEnd = true;
-        }
-        
-        if (curr->left) {
-            q.push(curr->left);
-        }
-        
-        if (curr->right) {
-            q.push(curr->right);
-        }
-    }
-    
-    return true;
-}
+};
