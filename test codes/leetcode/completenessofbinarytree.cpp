c0dd3f475280ede6f4af325a04a322318efbdf2a
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-03-15 23:56:45 +0530

    25

--- test codes/leetcode/completenessofbinarytree.cpp
+++ test codes/leetcode/completenessofbinarytree.cpp
@@ -1,6 +1,47 @@
-class Solution {
-public:
-    bool isCompleteTree(TreeNode* root) {
-        
-    }
+#include <queue>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
+
+bool isCompleteTree(TreeNode* root) {
+    if (!root) {
+        return true;
+    }
+    
+    queue<TreeNode*> q;
+    q.push(root);
+    
+    bool reachedEnd = false;
+    while (!q.empty()) {
+        TreeNode* curr = q.front();
+        q.pop();
+        
+        if (reachedEnd && (curr->left || curr->right)) {
+            return false;
+        }
+        
+        if (!curr->left && curr->right) {
+            return false;
+        }
+        
+        if ((curr->left && !curr->right) || (!curr->left && !curr->right)) {
+            reachedEnd = true;
+        }
+        
+        if (curr->left) {
+            q.push(curr->left);
+        }
+        
+        if (curr->right) {
+            q.push(curr->right);
+        }
+    }
+    
+    return true;
+}
