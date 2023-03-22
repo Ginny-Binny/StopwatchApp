c9f37a164ea055103fbdbb879b1dd5005d9b0886
GaurangSharma2001 <gaurangsh9897@gmail.com>
2023-03-23 02:39:28 +0530

    32

--- test codes/leetcode/minmscoreofpathb/w2cities.cpp
+++ test codes/leetcode/minmscoreofpathb/w2cities.cpp
@@ -1,28 +1,26 @@
-class Solution:
-    def bfs(self, n: int, adj: List[List[Tuple[int, int]]]) -> int:
-        visited = [False] * (n+1)
-        q = deque()
-        answer = inf
-
-        q.append(1)
-        visited[1] = True
-
-        while q:
-            node = q.popleft()
-
-            for edge in adj[node]:
-                answer = min(answer, edge[1])
-                if not visited[edge[0]]:
-                    visited[edge[0]] = True
-                    q.append(edge[0])
-
-        return answer
-
-    def minScore(self, n: int, roads: List[List[int]]) -> int:
-        adj = [[] for _ in range(n+1)]
-
-        for road in roads:
-            adj[road[0]].append((road[1], road[2]))
-            adj[road[1]].append((road[0], road[2]))
-
-        return self.bfs(n, adj)
+class Solution {
+    List<int[]> tree[];
+    boolean vis[];
+    Integer ans;
+    public int minScore(int n, int[][] roads) {
+        tree = new ArrayList[n+1];
+        vis = new boolean[n+1];
+        ans = Integer.MAX_VALUE;
+        for(int i=0;i<=n;i++) tree[i] = new ArrayList<>();
+        for(int road[]: roads) {
+            tree[road[0]].add(new int[]{road[1], road[2]});
+            tree[road[1]].add(new int[]{road[0], road[2]});
+        }
+        dfs(1, 0);
+        return ans;
+    }
+    public void dfs(int cur, int par) {
+        if(vis[cur]) return;
+        vis[cur] = true;
+        for(int child[]: tree[cur]) {
+            if(child[0] == par) continue;
+            ans = Math.min(ans, child[1]);
+            dfs(child[0], cur);
+        }
+    }
+}
